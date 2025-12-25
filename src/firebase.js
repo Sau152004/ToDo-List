69c7738f888cd45f58f85248bab76971a7b9faec
sau152004 <ysaurabh0704@gmail.com>
2025-12-25 16:32:05 +0530

    update firebase.js file

--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,4 @@
 // // Firebase core
-// import { initializeApp } from "firebase/app";
-
-// // Firebase services you need
-// import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-
-// // ✅ Your REAL Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDjfsPPxpomZsi2E1aEt-vwcj8qgoDQAE4",
-//   authDomain: "todo-productivity-app-58cee.firebaseapp.com",
-//   projectId: "todo-productivity-app-58cee",
-//   storageBucket: "todo-productivity-app-58cee.appspot.com",
-//   messagingSenderId: "112554802846",
-//   appId: "1:112554802846:web:21ddd1df5ee1b51d52458f",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// // ✅ Export services you’ll actually use
-// export const auth = getAuth(app);
-// export const provider = new GoogleAuthProvider();
-// export const db = getFirestore(app);
 
 
 import { initializeApp } from "firebase/app";
@@ -29,16 +6,16 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjfsPPxpomZsi2E1aEt-vwcj8qgoDQAE4",
-  authDomain: "todo-productivity-app-58cee.firebaseapp.com",
-  projectId: "todo-productivity-app-58cee",
-  storageBucket: "todo-productivity-app-58cee.appspot.com",
-  messagingSenderId: "112554802846",
-  appId: "1:112554802846:web:21ddd1df5ee1b51d52458f",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider(); // ✅ FIX
+export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
